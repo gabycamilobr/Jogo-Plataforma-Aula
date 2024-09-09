bd5514912ebc17a293acbc7918a22850b23ed0af
Gabrielly Barbosa Camilo <gabycamilobr@gmail.com>
2024-09-09 10:12:07 -0300

    Ajustes Final

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    [SerializeField] private float speed = 8f;
     private float horizontal;
     private Rigidbody2D rb;
     private bool isFacingRight = true;
     public Animator animator;
 
+
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
+        this.rb = this.GetComponent<Rigidbody2D>();
+        animator = this.GetComponent<Animator>();
     }
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
         Debug.Log(horizontal);
-        this.rb.velocity = new Vector2(horizontal * 8, rb.velocity.y);
+        this.rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+      
         animator.SetFloat("speed", Mathf.Abs(horizontal));
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.rb.AddForce(Vector2.up * 10f, ForceMode2D.Impulse);
+        }
+
         Flip();
 
        /*f (Input.GetKey(KeyCode.Space))
